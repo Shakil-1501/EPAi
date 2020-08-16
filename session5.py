e2b1dd865b28b9c55a7adf402c8402bc384f07f0
Shakil-1501 <63920152+Shakil-1501@users.noreply.github.com>
2020-08-16 16:32:38 +0530

    Add files via upload

---
+++ session5.py
@@ -0,0 +1,44 @@
+from typing import List
+import time
+import sys
+import weakref
+import random
+from math import tan, pi
+
+def temp_convertor(temp,temp_given_in = 'f'):
+    if temp_given_in =='f' and temp>=0:
+        t= (temp-32) * 5/9
+    elif temp_given_in =='c' and temp>=0:
+        t=(temp*(9/5))+32
+    elif temp<0 and temp_given_in not in ('c','f'):
+        t="please select valid range and correct scale"  
+    elif temp<0:
+        t="please enter the valid range"
+    elif temp_given_in not in ('c','f'):
+        t="please select correct scale"
+    
+    return t
+
+
+def polygon_area(side_length,side=3):
+    if side_length>0 and 3<=side<=6:
+        area=round(side * (side_length ** 2) / (4 * tan(pi /side)),4)
+    elif side_length<=0 and (side<3 or side>6):
+        area="please enter valid side and side_length"
+    elif side_length<=0:
+        area="please enter valid side_length"
+    elif side<3 or side>6:
+        area="please enter side in range 3 to 6"
+        
+    return area
+
+def squared_power_list(number,start=0,end=5):
+    a=list()
+    if number>0 and start<end:
+        for i in range(start,end+1):
+            a.append(number**i)
+    elif start>end:
+        a.append("start value should be less than end")
+    else:
+        a.append("select valid number")
+    return a 
---
+++ test_session5.py
@@ -0,0 +1,69 @@
+import pytest , os , session5 , inspect , re , random
+from decimal import Decimal
+import math
+import cmath
+
+
+def test_temp_convertor_to_c():
+    q1=session5.temp_convertor(212,temp_given_in = 'f')
+    assert q1==100, 'Wrong value fetched'
+
+
+def test_temp_convertor_to_f():
+    q1=session5.temp_convertor(100,temp_given_in = 'c')
+    assert q1==212, 'Wrong value fetched'
+
+
+def test_valid_temperature_scale():
+    q1=session5.temp_convertor(100,temp_given_in = 'd')
+    assert q1=='please select correct scale'
+
+
+def test_valid_temperature_range():
+    q1=session5.temp_convertor(-5,temp_given_in = 'c')
+    assert q1=='please enter the valid range'
+
+
+def test_valid_temperature_range_and_scale():
+    q1=session5.temp_convertor(-5,temp_given_in = 'd')
+    assert q1=='please select valid range and correct scale'
+
+
+def test_polygon_area():
+    q1=session5.polygon_area(3,side=4)
+    assert q1==9.0,'Wrong area calculated'
+
+
+def test_polygon_side_length():
+    q1=session5.polygon_area(-2,side=4)
+    assert q1=='please enter valid side_length'
+
+
+def test_polygon_side():
+    q1=session5.polygon_area(3,side=1)
+    assert q1=='please enter side in range 3 to 6'
+
+def test_valid_side_and_length():
+    q1=session5.polygon_area(-2,side=1)
+    assert q1=='please enter valid side and side_length'
+
+'''
+def test_print_func():
+    assert print(1, 2, 3, sep='-', end= ' ***\n')=='1-2-3 ***'
+    
+'''
+
+
+def test_squared_power_list():
+    q1=session5.squared_power_list(2,start=0,end=5)
+    assert q1==[1, 2, 4, 8, 16, 32],'Wrong Result'
+
+
+def number_check_in_test_squared():
+    q1=session5.squared_power_list(-2,start=0,end=5)
+    assert q1==['select valid number'],'Wrong Result'
+
+
+def start_and_end_value():
+    q1=session5.squared_power_list(2,start=2,end=1)
+    assert q1==['start value should be less than end']
