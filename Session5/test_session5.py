32e7c0acf041528e2e0ed93a7809bae1380227c2
Shakil-1501 <63920152+Shakil-1501@users.noreply.github.com>
2020-08-17 20:28:41 +0530

    Add files via upload

--- Session5/test_session5.py
+++ Session5/test_session5.py
@@ -108,27 +108,27 @@ def test_print_function():
 
 
 def test_timeit_temp_convertor():
-    q1=session5.time_it('temp_convertor',212,  repetitons= 100,temp_given_in = 'f')
+    q1=session5.time_it(temp_convertor, 212,  repetitons= 100,temp_given_in = 'f')
     assert type(q1) is float
 
 
 def test_timeit_polygon_area():
-    q1=session5.time_it(polygon_area,3,repetitons=100,side=3)
+    q1=session5.time_it(polygon_area, 3, repetitons=100,side=3)
     assert type(q1) is float
 
 
 def test_time_it_speed_convertor():
-    q1=session5.time_it(speed_convertor,20,repetitons=100,dist='m', time='s')
+    q1=session5.time_it(speed_convertor, 20 , repetitons=100,dist='m', time='s')
     assert type(q1) is float
 
 
 def test_timeit_squared_powerlist():
-    q1=session5.time_it(squared_power_list,2,repetitons=100,start=0,end=5)
+    q1=session5.time_it(squared_power_list, 2 , repetitons=100,start=0,end=5)
     assert type(q1) is float
 
 
 def test_time_it_print():
-    q1=session5.time_it(print, 1, 2, 3,repetitons=100,sep='-', end= ' ***\n')
+    q1=session5.time_it(print, 1, 2, 3,repetitons=100 , sep='-', end= ' ***\n')
     assert type(q1) is float
 
 
